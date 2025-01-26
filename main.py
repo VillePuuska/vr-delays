69edff32b68ed46390db56bedbb2b6e37d9a844b
Ville Puuska <puuskaville@gmail.com>
2025-01-26 11:07:40 +0200

    init fetching train schedule/delay data from digitraffic API

---
+++ main.py
@@ -0,0 +1,37 @@
+from utils.fetch import fetch_train_for_date
+from typing import Any
+import datetime
+import json
+import asyncio
+
+
+async def main():
+    response_json = await fetch_train_for_date(
+        train=40, date=datetime.date(year=2025, month=1, day=24)
+    )
+    assert isinstance(response_json, list)
+
+    if len(response_json) == 0:
+        print("No data for the day. Is the train scheduled for said day?")
+        return
+    assert len(response_json) == 1, "a train-line is assumed to only run once per day"
+
+    time_table_rows: list[dict[str, Any]] = response_json[0]["timeTableRows"]
+    tpe_departure = [
+        row
+        for row in time_table_rows
+        if row["stationShortCode"] == "TPE" and row["type"] == "DEPARTURE"
+    ][0]
+    psl_arrival = [
+        row
+        for row in time_table_rows
+        if row["stationShortCode"] == "PSL" and row["type"] == "ARRIVAL"
+    ][0]
+
+    print(f"{response_json[0]['trainType']} {response_json[0]['trainNumber']}")
+    print(json.dumps(tpe_departure, indent=2))
+    print(json.dumps(psl_arrival, indent=2))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ utils/fetch.py
@@ -0,0 +1,17 @@
+from typing import Any
+import polars as pl
+import datetime
+import aiohttp
+
+
+async def fetch_train_for_date(train: int, date: datetime.date) -> list[dict[str, Any]]:
+    date_formatted = date.strftime(format="%Y-%m-%d")
+    async with aiohttp.ClientSession() as session:
+        response = await session.request(
+            method="GET",
+            url=f"https://rata.digitraffic.fi/api/v1/trains/{date_formatted}/{train}",
+        )
+        response.raise_for_status()
+
+        response_json = await response.json()
+    return response_json
