9e025bf8b1ce27482e00569aa7585489a6e51b54
Ville Puuska <puuskaville@gmail.com>
2025-01-26 14:19:53 +0200

    pydantic model for API response (only needed fields)

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 from utils.fetch import fetch_train_for_date
-from typing import Any
 import datetime
-import json
 import asyncio
 
 
 async def main() -> None:
-    response_json = await fetch_train_for_date(
+    response = await fetch_train_for_date(
         train=40, date=datetime.date(year=2025, month=1, day=24)
     )
-    assert isinstance(response_json, list)
 
-    if len(response_json) == 0:
+    if len(response.root) == 0:
         print("No data for the day. Is the train scheduled for said day?")
         return
-    assert len(response_json) == 1, "a train-line is assumed to only run once per day"
+    assert len(response.root) == 1, "a train-line is assumed to only run once per day"
 
-    time_table_rows: list[dict[str, Any]] = response_json[0]["timeTableRows"]
+    time_table_rows = response.root[0].time_table_rows
     tpe_departure = [
         row
-        for row in time_table_rows
-        if row["stationShortCode"] == "TPE" and row["type"] == "DEPARTURE"
+        for row in time_table_rows.root
+        if row.station_short_code == "TPE" and row.type == "DEPARTURE"
     ][0]
     psl_arrival = [
         row
-        for row in time_table_rows
-        if row["stationShortCode"] == "PSL" and row["type"] == "ARRIVAL"
+        for row in time_table_rows.root
+        if row.station_short_code == "PSL" and row.type == "ARRIVAL"
     ][0]
 
-    print(f"{response_json[0]['trainType']} {response_json[0]['trainNumber']}")
-    print(json.dumps(tpe_departure, indent=2))
-    print(json.dumps(psl_arrival, indent=2))
+    print(f"{response.root[0].train_type} {response.root[0].train_number}")
+    print(tpe_departure)
+    print(psl_arrival)
 
 
 if __name__ == "__main__":
--- utils/fetch.py
+++ utils/fetch.py
@@ -1,9 +1,37 @@
-from typing import Any
+from pydantic import BaseModel, Field, RootModel
 import datetime
 import aiohttp
 
 
-async def fetch_train_for_date(train: int, date: datetime.date) -> list[dict[str, Any]]:
+class TimeTableRow(BaseModel):
+    station_short_code: str = Field(validation_alias="stationShortCode")
+    type: str
+    cancelled: bool
+    scheduled_time: datetime.datetime = Field(validation_alias="scheduledTime")
+    actual_time: datetime.datetime | None = Field(
+        default=None, validation_alias="actualTime"
+    )
+    difference_in_minutes: int | None = Field(
+        default=None, validation_alias="differenceInMinutes"
+    )
+
+
+class TimeTableRows(RootModel):
+    root: list[TimeTableRow] = Field(default_factory=list)
+
+
+class Train(BaseModel):
+    train_number: int = Field(validation_alias="trainNumber")
+    departure_date: datetime.date = Field(validation_alias="departureDate")
+    train_type: str = Field(validation_alias="trainType")
+    time_table_rows: TimeTableRows = Field(validation_alias="timeTableRows")
+
+
+class TrainList(RootModel):
+    root: list[Train] = Field(default_factory=list)
+
+
+async def fetch_train_for_date(train: int, date: datetime.date) -> TrainList:
     date_formatted = date.strftime(format="%Y-%m-%d")
     async with aiohttp.ClientSession() as session:
         response = await session.request(
@@ -12,5 +40,5 @@ async def fetch_train_for_date(train: int, date: datetime.date) -> list[dict[str
         )
         response.raise_for_status()
 
-        response_json = await response.json()
-    return response_json
+        response_json = await response.text()
+    return TrainList.model_validate_json(json_data=response_json)
