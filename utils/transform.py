3a13576cfb05331fc7417da68fece733de42e39e
Ville Puuska <puuskaville@gmail.com>
2025-01-26 17:43:07 +0200

    quick look at train schedules between Tampere and Pasila

--- utils/transform.py
+++ utils/transform.py
@@ -19,22 +19,40 @@ def train_list_to_dataframe(
             for row in train.time_table_rows.root
             if row.station_short_code == departure and row.type == "DEPARTURE"
         ]
-        assert len(dep_rows) == 1
+
+        if len(dep_rows) != 1:
+            continue
+
         dep_row = dep_rows[0]
         row["departure_scheduled_time"] = dep_row.scheduled_time
         row["departure_actual_time"] = dep_row.actual_time
         row["departure_difference_in_minutes"] = dep_row.difference_in_minutes
+        if dep_row.actual_time is not None:
+            row["departure_actual_difference"] = (
+                dep_row.actual_time - dep_row.scheduled_time
+            ).total_seconds() / 60.0
+        else:
+            row["departure_actual_difference"] = None
 
         arr_rows = [
             row
             for row in train.time_table_rows.root
             if row.station_short_code == arrival and row.type == "ARRIVAL"
         ]
-        assert len(arr_rows) == 1
+
+        if len(arr_rows) != 1:
+            continue
+
         arr_row = arr_rows[0]
         row["arrival_scheduled_time"] = arr_row.scheduled_time
         row["arrival_actual_time"] = arr_row.actual_time
         row["arrival_difference_in_minutes"] = arr_row.difference_in_minutes
+        if arr_row.actual_time is not None:
+            row["arrival_actual_difference"] = (
+                arr_row.actual_time - arr_row.scheduled_time
+            ).total_seconds() / 60.0
+        else:
+            row["arrival_actual_difference"] = None
 
         rows.append(row)
 
